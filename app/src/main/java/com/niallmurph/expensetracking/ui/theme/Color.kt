1d0b859faaae3883bc55f397ddd4742014e994b9
Niall <niall@Aarons-MacBook-Air.local>
2023-11-24 11:12:06 +0000

    parking for switch to alternate branch

--- app/src/main/java/com/niallmurph/expensetracking/ui/theme/Color.kt
+++ app/src/main/java/com/niallmurph/expensetracking/ui/theme/Color.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Surface = Color.Black
 val PrimaryText = Color.White
 val BackgroundElevated = Color(0xFF1C1C1E)
+val Destructive = Color(0xFFFF453A)
 val Primary = Color(0xFF0A84FF)
 val PrimaryVariant = Color(0xFF5E5CE6)
 val OnPrimary = Color.White
